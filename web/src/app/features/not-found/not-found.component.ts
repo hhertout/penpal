57b2f1e528759786db3487cde9538fce34d65d2e
hhertout <hugues.hertout@gmail.com>
2025-06-01 10:49:13 +0200

    adding more stuff

--- web/src/app/features/not-found/not-found.component.ts
+++ web/src/app/features/not-found/not-found.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-not-found',
-  imports: [],
+  imports: [MatIcon],
   templateUrl: './not-found.component.html',
-  styleUrl: './not-found.component.css'
+  styleUrl: './not-found.component.css',
 })
-export class NotFoundComponent {
-
-}
+export class NotFoundComponent {}
